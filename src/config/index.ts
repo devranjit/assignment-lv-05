e11c1389ea6e104ce62b8ab266b95fa23d8ee5a7
devranjit <dvranjitplays@gmail.com>
2025-08-01 22:35:04 +0600

    hosting issue fixing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,9 @@
+import serverless from 'serverless-http';
+import expressApp from '../app';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const handler = serverless(expressApp);
 export const PORT = process.env.PORT || 5000;
 export const MONGO_URI = process.env.MONGO_URI as string;
